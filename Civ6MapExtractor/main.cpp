06080243d970fe02d7229c1d0bbca94c09b2c0d8
Jacob Michaelis <jacob.c.michaelis@gmail.com>
2022-01-05 00:42:04 -0800

    add the sqlite library

---
+++ Civ6MapExtractor/main.cpp
@@ -0,0 +1,7 @@
+
+#pragma comment(lib, "sqlite3-static.lib")
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
---
+++ Civ6MapExtractor/typedefs.h
@@ -0,0 +1,19 @@
+#pragma once
+//#pragma warning( disable :  )
+
+#define _USE_MATH_DEFINES
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdint.h>
+
+typedef char           int8;
+typedef short          int16;
+typedef int            int32;
+typedef unsigned char  uint8;
+typedef unsigned short uint16;
+typedef unsigned       uint32;
+typedef uint64_t       uint64;
+typedef float          float32;
+typedef double         float64;
+
+#define STATIC_ASSERT(expr) typedef char __static_assert_t[(expr) != 0]
